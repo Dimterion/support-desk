853ed11019962063cd11a78990133a00d8293db3
Dmitrii <dimterion59@gmail.com>
2025-06-02 11:36:34 +0200

    add initial ticket actions structure

--- src/components/TicketForm.tsx
+++ src/components/TicketForm.tsx
@@ -1,10 +1,12 @@
+import { createTicket } from "@/actions/ticket.actions";
+
 const TicketForm = () => {
   return (
     <section className="w-full max-w-md rounded-lg border border-gray-200 bg-white p-8 shadow-md">
       <h1 className="mb-6 text-center text-3xl font-bold text-blue-600">
         Submit a Support Ticket
       </h1>
-      <form className="space-y-4 text-gray-700">
+      <form action={createTicket} className="space-y-4 text-gray-700">
         <input
           className="w-full rounded border border-gray-200 p-3 focus:ring-2 focus:ring-blue-400 focus:outline-none"
           type="text"
