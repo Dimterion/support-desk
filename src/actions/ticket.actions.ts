ac2fed96a20cf68eeb9369462dfcd45eea96ace5
Dmitrii <dimterion59@gmail.com>
2025-06-03 15:46:25 +0200

    add useActionState to TicketForm component

--- src/actions/ticket.actions.ts
+++ src/actions/ticket.actions.ts
@@ -1,6 +1,9 @@
 "use server";
 
-export const createTicket = async (formData: FormData) => {
+export const createTicket = async (
+  prevState: { success: boolean; message: string },
+  formData: FormData,
+): Promise<{ success: boolean; message: string }> => {
   const subject = formData.get("subject") as string;
   const description = formData.get("description") as string;
   const priority = formData.get("priority") as string;
