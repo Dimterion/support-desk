1b0eaebe5e10c122bf5702e26171ac662c8aa784
Dimterion <dimterion59@gmail.com>
2025-06-06 10:58:49 +0200

    adjust tickets page

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,17 @@
-const TicketsPage = () => {
-  return <div>TicketsPage</div>;
+import Link from "next/link";
+import { getTickets } from "@/actions/ticket.actions";
+import { logEvent } from "@/utils/sentry";
+
+const TicketsPage = async () => {
+  const tickets = await getTickets();
+
+  return (
+    <main className="min-h-screen bg-blue-50 p-8">
+      <h1 className="mb-8 text-center text-3xl font-bold text-blue-600">
+        Support Tickets
+      </h1>
+    </main>
+  );
 };
 
 export default TicketsPage;
