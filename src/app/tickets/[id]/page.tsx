ae8e2373ca98722c3a6a18e13fe019baf1ebf6cf
Dimterion <dimterion59@gmail.com>
2025-06-08 09:09:51 +0300

    adjust TicketDetailsPage

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -2,9 +2,14 @@ import { getTicketById } from "@/actions/ticket.actions";
 import { logEvent } from "@/utils/sentry";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { getPriorityClass } from "@/utils/ui";
 
-const TicketDetailsPage = () => {
-  return <div>TicketDetailsPage</div>;
+const TicketDetailsPage = async (props: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await props.params;
+
+  return <main>Ticket Details {id}</main>;
 };
 
 export default TicketDetailsPage;
