2a3925edfe3c7d15afdd84109dd297a20b50244d
Dimterion <dimterion59@gmail.com>
2025-06-10 09:01:35 +0300

    adjust Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,25 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between border-b border-gray-200 bg-white px-6 py-4">
-      <div>
+      <section>
         <Link href="/" className="text-xl font-bold text-blue-600">
           Support Desk
         </Link>
-      </div>
+      </section>
+      <section className="flex items-center space-x-4">
+        <Link
+          href="/tickets/new"
+          className="text-gray-700 transition hover:underline"
+        >
+          New Ticket
+        </Link>
+        <Link
+          href="/tickets"
+          className="text-gray-700 transition hover:underline"
+        >
+          My Tickets
+        </Link>
+      </section>
     </nav>
   );
 };
