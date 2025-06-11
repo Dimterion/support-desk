fce492065dcdc8e1c6d25fcd5f1ba16d2e73961d
Dimterion <dimterion59@gmail.com>
2025-06-11 09:03:48 +0300

    add LogoutBtn component

---
+++ src/components/LogoutBtn.tsx
@@ -0,0 +1,14 @@
+const LogoutBtn = () => {
+  return (
+    <form>
+      <button
+        type="submit"
+        className="rounded bg-red-500 px-4 py-2 text-white transition hover:bg-red-600"
+      >
+        Logout
+      </button>
+    </form>
+  );
+};
+
+export default LogoutBtn;
