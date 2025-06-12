9fdea9500efe10c44fcb5831382f1d8d4a2c0edb
Dimterion <dimterion59@gmail.com>
2025-06-12 08:58:10 +0300

    adjust CloseTicketBtn component

--- src/components/CloseTicketBtn.tsx
+++ src/components/CloseTicketBtn.tsx
@@ -1,6 +1,33 @@
-const CloseTicketBtn = () => {
+"use client";
+
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
+
+const CloseTicketBtn = ({
+  isClosed,
+}: {
+  ticketId: number;
+  isClosed: boolean;
+}) => {
+  const initialState = {
+    success: false,
+    message: "",
+  };
+
+  const [state, formAction] = useActionState(closeTicket, initialState);
+
+  useEffect(() => {
+    if (state.success) {
+      toast.success(state.message);
+    } else if (state.message && !state.success) {
+      toast.error(state.message);
+    }
+  }, [state]);
+
+  if (isClosed) return null;
+
   return (
-    <form>
+    <form action={formAction}>
       <input type="hidden" name="ticketId" />
       <button
         type="submit"
