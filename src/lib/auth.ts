b009122c817a5de3ffe8f84aa23c6726ac7d65ea
Dimterion <dimterion59@gmail.com>
2025-06-13 08:45:26 +0300

    add signAuthToken function

---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+import { logEvent } from "@/utils/sentry";
+
+const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
+const cookieName = "auth-token";
+
+export const signAuthToken = async (payload: any) => {
+  try {
+    const token = await new SignJWT(payload)
+      .setProtectedHeader({ alg: "HS256" })
+      .setIssuedAt()
+      .setExpirationTime("7d")
+      .sign(secret);
+
+    return token;
+  } catch (error) {
+    logEvent("Token signing failed", "auth", { payload }, "error", error);
+
+    throw new Error("Token signing failed");
+  }
+};
