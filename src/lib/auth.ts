7c9e2eebfb8ddbcac8aa87903962ab926c59a4b6
Dimterion <dimterion59@gmail.com>
2025-06-13 08:52:28 +0300

    add verifyAuthToken function

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,11 @@ import { SignJWT, jwtVerify } from "jose";
 import { cookies } from "next/headers";
 import { logEvent } from "@/utils/sentry";
 
+// Encrypt and sign token
 const secret = new TextEncoder().encode(process.env.AUTH_SECRET);
 const cookieName = "auth-token";
 
-export const signAuthToken = async (payload: any) => {
+export async function signAuthToken(payload: any) {
   try {
     const token = await new SignJWT(payload)
       .setProtectedHeader({ alg: "HS256" })
@@ -19,4 +20,23 @@ export const signAuthToken = async (payload: any) => {
 
     throw new Error("Token signing failed");
   }
-};
+}
+
+// Decrypt and verify token
+export async function verifyAuthToken<T>(token: string): Promise<T> {
+  try {
+    const { payload } = await jwtVerify(token, secret);
+
+    return payload as T;
+  } catch (error) {
+    logEvent(
+      "Token decryption failed",
+      "auth",
+      { tokenSnippet: token.slice(0, 10) },
+      "error",
+      error,
+    );
+
+    throw new Error("Token decryption failed");
+  }
+}
