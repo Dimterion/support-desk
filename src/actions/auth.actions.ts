c9c1c7ec4e6449da5383a2cdcdff00fd9e18782d
Dimterion <dimterion59@gmail.com>
2025-06-14 09:04:00 +0300

    adjust registerUser function

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -15,7 +15,20 @@ export async function registerUser(
   prevState: ResponseResult,
   formData: FormData,
 ): Promise<ResponseResult> {
-  const name = formData.get("name") as string;
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
+  try {
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    if (!name || !email || !password) {
+      logEvent(
+        "Validation error: Missing register fields",
+        "auth",
+        { name, email },
+        "warning",
+      );
+
+      return { success: false, message: "All fields are required" };
+    }
+  } catch (error) {}
 }
