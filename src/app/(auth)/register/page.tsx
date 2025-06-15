2f1934f0cc79f79c69ad95707109e23c858e08cd
Dimterion <dimterion59@gmail.com>
2025-06-15 08:52:01 +0300

    add RegisterPage

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/router";
+import { registerUser } from "@/actions/auth.actions";
+
+const RegisterPage = () => {
+  return <div>RegisterPage</div>;
+};
+
+export default RegisterPage;
