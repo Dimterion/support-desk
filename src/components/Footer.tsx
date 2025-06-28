7a402cc2cfe46089365e54f81a6466e35fa260ba
Dmitrii <dimterion59@gmail.com>
2025-06-28 12:45:01 +0200

    adjust footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,15 @@
-const Footer = async () => {
-  return <footer className="text-center">Dimterion</footer>;
+const Footer = () => {
+  return (
+    <footer className="text-center text-xs">
+      <a
+        href="https://github.com/Dimterion/support-desk"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        &copy; {new Date().getFullYear()} Dimterion
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
